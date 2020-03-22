66a10c0a265e57544f0d11e41a34b204b3dc38d4
YvanKostenevich <YvanKostenevich@gmail.com>
2020-03-22 23:09:50 +0300

    feat: comlete typical array problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined) {
+        return 0
+    }
+    if(array.length === 0){return 0}
+    return array ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined) {
+        return 0
+    }
+    if(array.length === 0){return 0}
+    return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined) {
+        return 0
+    }
+    if(array.length === 0){return 0}
+    return array.reduce((x, y) => x + y) / array.length;
 }
